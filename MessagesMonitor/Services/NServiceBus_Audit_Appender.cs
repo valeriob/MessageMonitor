07abd5f22c2f2a48e35174e20d0d4a09e36ec651
Valeriob <valeriob@gmail.com>
2012-08-30 23:03:53 +0200

    inizio query audit

--- MessagesMonitor/Services/NServiceBus_Audit_Appender.cs
+++ MessagesMonitor/Services/NServiceBus_Audit_Appender.cs
@@ -21,5 +21,29 @@ namespace MessageMonitor.Services
             Session.Store(message);
             Session.SaveChanges();
         }
+
+        public IEnumerable<Audit_Message_SinteticDto> Query(Browse_Audit query)
+        {
+            return Session.Query<Audit_Message_SinteticDto>()
+                .ToList();
+        }
+    }
+
+    public class Browse_Audit
+    {
+        public DateTime? From { get; set; }
+        public DateTime? UpTo { get; set; }
+
+        public string Queue { get; set; }
+        public string Computer { get; set; }
+    }
+    public class Audit_Message_SinteticDto
+    {
+        public string XmlBody { get; set; }
+        public List<Header> Headers { get; set; }
+
+        public string MessageIntent { get; set; }
+        public string ReplyToAddress { get; set; }
+        public DateTime? TimeSent { get; set; }
     }
 }
