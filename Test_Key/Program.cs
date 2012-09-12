308cdc6e4248d08147a796f3c87b47ced0dc5060
Valerio Borioni <valeriob@gmail.com>
2012-09-12 17:45:08 +0200

    bench project for keys

---
+++ Test_Key/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test_Key
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10000000;
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            IKey key1 = new Key { Value = BitConverter.GetBytes(int.MaxValue) };
+            IKey key2 = new Key { Value = BitConverter.GetBytes(0) };
+
+            int v1 = int.MaxValue;
+            int v2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+               // bool value = key1.Equals(key2);
+                bool value = v1.Equals(v2);
+            }
+
+            watch.Stop();
+
+            Console.WriteLine("elapsed" + watch.Elapsed);
+            Console.ReadLine();
+        }
+    }
+
+    public interface IKey 
+    {
+        byte[] Value { get; }
+    }
+    public struct Key : IEquatable<Key>, IKey
+    {
+        public byte[] Value { get; set; }
+
+
+        public bool Equals(Key other)
+        {
+            for (int i = 0; i < Value.Length; i++)
+                if (Value[i] != other.Value[i])
+                    return false;
+            return true;
+        }
+
+    }
+}
